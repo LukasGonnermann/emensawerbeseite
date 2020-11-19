cf1a18401daa4667dfae98a26c33c15f9b5dcdf2
Hamdy Sarhan <hamdyabdallah565@outlook.com>
2020-11-20 00:13:10 +0100

    Aufgabe 2,6,7 fertig

--- werbeseite/nl-admin.php
+++ werbeseite/nl-admin.php
@@ -1,60 +1,127 @@
+<?php
+/**
+ * Praktikum DBWT. Autoren:
+ * Lukas, Gonnermann, 3218299
+ * Hamdy, Sarhan, 3251443
+ */
 
-<html>
+$file = file('gespeichert.csv');
+const GET_PARAM_SEARCH_NAME = 'ByName';
+const GET_PARAM_SEARCH_EMAIL = 'ByEmail';
+const GET_PARAM_NAME = 'SearchName';
+
+foreach($file as $zeile) {
+    $sort[] = explode(",",$zeile);
+}
+
+$name = array_column($sort, 1);
+$email = array_column($sort, 2);
+if(isset($_GET[GET_PARAM_NAME]))
+{
+    $G = $_GET[GET_PARAM_NAME];
+}
+?>
+<!DOCTYPE html>
+<html lang="de">
 <head>
     <meta charset="UTF-8">
+    <title>nl_admin</title>
+    <style type="text/css">
+        table, th, td {
+            border: 1px solid black;
+        }
 
-
-    <title>nl-admin</title>
+    </style>
 </head>
 <body>
-<form method="post">
-    <legend>Gespeicherte Daten</legend><br>
-    <br>
-    <input id="submit1" name="submit1" type="submit" value="Sortierung nach Name">
+<form method="get">
+    <input type="submit" name="ByName" value="Sortierung nach Name">
     <br>
     <br>
-    <input id="submit2" name="submit2" type="submit" value="Sortierung nach Email">
-    <br>
-
-
-
-
+    <input type="submit" name="ByEmail" value="Sortierung nach E-Mail">
 </form>
+<br>
+<form method="get">
 
+    <label for="search_name">Search a name:</label>
+    <input id="search_text" type="text" name="SearchName" value="<?=htmlspecialchars($G);?>">
+
+    <input type="submit" value="Search">
+    <table>
+        <th>Nummer</th> <th>Name</th> <th>E-Mail</th> <th>Sprache</th> <th>Datenschutzstatus</th>
+        <tr>
+            <?php
+
+            $filter = [];
+            if(isset($G))
+            {
+                $searchName = $G;
+                foreach ($sort as $form) {
+                    if (strpos(strtolower($form[1]), strtolower($G)) !== false) {
+                        $filter[] = $form;
+                    }
+                }
+
+
+                foreach ($filter as $element) {
+                    echo "<tr>",
+                        "<td>$element[0]</td>".
+                        "<td>$element[1]</td>".
+                        "<td>$element[2]</td>".
+                        "<td>$element[3]</td>".
+                        "<td>$element[4]</td>".
+                        "</tr>";
+                }
+
+
+                '</tr>';
+
+                '<tr>';
+            }
+            else  {
+
+
+                foreach ($sort as $form) {
+                    if (!isset($_GET[GET_PARAM_SEARCH_NAME]) && !isset($_GET[GET_PARAM_SEARCH_EMAIL])) {
+                        echo "<tr>",
+                            "<td>$form[0]</td>" .
+                            "<td>$form[1]</td>" .
+                            "<td>$form[2]</td>" .
+                            "<td>$form[3]</td>" .
+                            "<td>$form[4]</td>" .
+                            "</tr>";
+                    }
+                }
+                if (isset($_GET[GET_PARAM_SEARCH_NAME])) {
+                    array_multisort($name, SORT_STRING, $sort);
+                    foreach ($sort as $form) {
+                        echo "<tr>",
+                            "<td>$form[0]</td>" .
+                            "<td>$form[1]</td>" .
+                            "<td>$form[2]</td>" .
+                            "<td>$form[3]</td>" .
+                            "<td>$form[4]</td>" .
+
+
+                            "</tr>";
+                    }
+                }
+                if (isset($_GET[GET_PARAM_SEARCH_EMAIL])) {
+                    array_multisort($email, SORT_STRING, $sort);
+                    foreach ($sort as $form) {
+                        echo "<tr>",
+                            "<td>$form[0]</td>" .
+                            "<td>$form[1]</td>" .
+                            "<td>$form[2]</td>" .
+                            "<td>$form[3]</td>" .
+                            "<td>$form[4]</td>" .
+                            "</tr>";
+                    }
+                }
+            }
+            ?>
+        </tr>
+    </table>
+</form>
 </body>
 </html>
-
-<?php
-function readCSV($file)
-{
-    $row      = 0;
-    $csvArray = array();
-    if( ( $handle = fopen($file, "r") ) !== FALSE ) {
-        while( ( $data = fgetcsv($handle, 0, ",") ) !== FALSE ) {
-            $num = count($data);
-            for( $c = 0; $c < $num; $c++ ) {
-                $csvArray[$row][] = $data[$c];
-            }
-            $row++;
-        }
-    }
-    if( !empty( $csvArray ) ) {
-        return array_splice($csvArray, 0); //cut off the first row (names of the fields)
-    } else {
-        return false;
-    }
-}
-
-$csvData = readCSV('gespeichert.csv');
-if(isset($_POST["submit1"])){
-
-    sort($csvData);
-    print_r($csvData);
-}
-elseif(isset($_POST["submit2"])){
-
-    asort($csvData);
-    print_r($csvData);
-}
-
-?>
