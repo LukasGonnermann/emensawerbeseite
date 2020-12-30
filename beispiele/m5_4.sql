28612eb6624611952a616caad2f0575153a140d2
Hamdy Sarhan <hamdyabdallah565@outlook.com>
2020-12-30 21:09:06 +0100

    Aufgabe 4 fast fertig

--- beispiele/m5_4.sql
+++ beispiele/m5_4.sql
@@ -1,17 +1,19 @@
 CREATE VIEW Suppen_Gerichte AS
-SELECT name
+SELECT *
         FROM emensawerbeseite.gericht
         WHERE name LIKE '%suppe%' or name LIKE '%Suppe%';
 
 CREATE VIEW view_anmeldungen AS
-SELECT anzahlanmeldungen
+SELECT *
 FROM emensawerbeseite.benutzer
 ORDER BY anzahlanmeldungen DESC ;
 
-CREATE VIEW view_kategoriegerichte_vegetarisch AS
-SELECT name , kategorie_id
-FROM emensawerbeseite.gericht, emensawerbeseite.gericht_hat_kategorie
-WHERE vegetarisch = 1  ;
+CREATE VIEW view_kategoriegerichte_vegetarisch AS SELECT DISTINCT emensawerbeseite.gericht.name AS gerichtname ,
+GROUP_CONCAT(emensawerbeseite.kategorie.name) AS Kategorie
+FROM emensawerbeseite.gericht LEFT JOIN emensawerbeseite.gericht_hat_kategorie on emensawerbeseite.gericht.id = gericht_hat_kategorie.gericht_id
+LEFT JOIN emensawerbeseite.kategorie ON gericht_hat_kategorie.kategorie_id = emensawerbeseite.kategorie.id
+WHERE gericht.vegetarisch =1
+GROUP BY gericht.name;
 
 
 
